e2c3947f8aad6ec2d4dc34bb9edc18ca3cade3e4
Simo Elalj <simo@elalj.com>
2023-04-10 13:02:39 +0200

    fix: fixed sorted with mixed cases

--- src/components/Table.js
+++ src/components/Table.js
@@ -218,7 +218,9 @@ export default class Table {
         } else if (columnDetails.filterType === 'compare') {
           el1 = Number(el1);
           el2 = Number(el2);
-        } else if (el1 instanceof String && el2 instanceof String) {
+        }
+
+        if (typeof el1 === 'string' && typeof el2 === 'string') {
           el1 = el1.toLowerCase();
           el2 = el2.toLowerCase();
         }
